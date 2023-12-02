e4e34df428a3ebe9a288f32a0b5b7a3256c8b144
hendrik <hendrik@hbook.local>
2023-12-02 21:48:38 +0100

    full periodic sync

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# s3-local-folder-sync
-Continuously sync a local folder and s3
+# Simple Sync between Local Folder and S3 Bucket
+
+This project provides a Python-based solution to continuously synchronize a local folder with an Amazon S3 bucket. It acts as a wrapper for the AWS CLI's sync command, extending its functionality with periodic syncing, custom initial sync direction, and enhanced logging.
+
+## Features
+
+- **Periodic Syncing:** Automatically syncs the local folder and the S3 bucket at configurable intervals.
+- **Flexible Initial Sync Direction:** Choose the initial sync direction (local-to-s3, s3-to-local, or auto) based on your requirements.
+- **Timestamp-Based Sync Strategy:** When set to auto, the script uses the last modified timestamps to determine the initial sync direction.
+- **Deletion Handling:** Reflects file deletions in both the local folder and the S3 bucket.
+- **Logging:** Detailed logs of the sync process, including any errors or important actions.
+
+## How It Works
+
+### Initial Sync
+
+The initial sync direction can be set via the `--initial-sync-source` argument to either `local`, `s3`, or `auto`. If set to `auto`, the script compares the last modified timestamps of files in both locations. The direction with the most recent changes is chosen as the source for the initial sync. This ensures that the latest modifications are always prioritized during the first synchronization.
+
+### Ongoing Sync Strategy
+
+After the initial sync, the script regularly checks for local changes and syncs them to the S3 bucket. This approach prioritizes local modifications, reflecting any new changes or deletions in the S3 bucket.
+
+### Deletion Handling
+
+This script ensures that deletions in the local folder are mirrored in the S3 bucket. However, it is important to note that deletions in S3 are not mirrored locally during the continuous sync. Instead, the local file replaces the deleted one in S3 in the next sync cycle.
+
+Consequently, if a file is deleted locally, this change will be synced to the S3 bucket, ensuring that deletions are mirrored. Conversely, if a file is deleted in S3, the local version of the file will be uploaded to S3 during the next sync, as the script starts syncing from local to S3 after the initial synchronization.
+
+If changes in the S3 bucket are ahead of the local state, you should start the script setting the initial sync direction to `s3-to-local`.
+
+### Logging
+
+The script provides detailed logging for each synchronization action, including successful syncs, file changes, and any errors encountered, which is important for troubleshooting and understanding the sync behavior.
+
+### Usage
+
+To use this script, ensure you have Poetry and AWS CLI installed and configured with the necessary access to the S3 bucket. Modify the configuration file `config.yml` to set your local folder path, S3 bucket name, sync intervals, and other preferences.
+
+Build the virtual environment:
+
+```bash
+poetry install
+```
+
+Run the script with Python, optionally specifying the initial sync direction:
+
+```bash
+poetry run python main.py --initial-sync-source auto
+```
+
+### Configuration
+
+The synchronization behavior can be customized via a configuration file. This includes setting the local folder path, S3 bucket name, sync intervals, AWS profile, and other options.
--- sync.py
+++ sync.py
@@ -1,10 +1,56 @@
 """Contains the logic for syncing files from local to S3 and vice versa."""
 
 import logging
+import os
 import subprocess
+import time
+from datetime import datetime, timezone
+
+import boto3
+
+import config
+
+cfg = config.load_config("config.yml")
+aws_profile = cfg.general.aws_profile
 
 
-def run_command(command):
+def get_last_modified_s3(s3_bucket_name: str) -> datetime:
+    """
+    Gets the last modified timestamp of the most recently updated file in an S3 bucket
+    """
+    s3 = boto3.client("s3")
+    last_modified = None
+
+    # List objects in the S3 bucket
+    response = s3.list_objects_v2(Bucket=s3_bucket_name)
+    for obj in response.get("Contents", []):
+        if last_modified is None or obj["LastModified"] > last_modified:
+            last_modified = obj["LastModified"]
+
+    return last_modified
+
+
+def get_last_modified_local(local_folder: str) -> datetime:
+    """
+    Gets the last modified timestamp of the most recently updated file in a local folder
+    """
+    last_modified = None
+
+    for root, dirs, files in os.walk(local_folder):
+        for name in files:
+            filepath = os.path.join(root, name)
+            mtime = os.path.getmtime(filepath)
+            mtime_dt = datetime.fromtimestamp(mtime, timezone.utc)
+            if last_modified is None or mtime_dt > last_modified:
+                last_modified = mtime_dt
+
+    return last_modified
+
+
+def run_command(command: str):
+    """
+    Executes a shell command and return its output
+    """
     try:
         process = subprocess.Popen(
             command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
@@ -17,13 +63,27 @@ def run_command(command):
         return None, None
 
 
-def sync_files(source: str, destination: str, delete: bool = True):
-    # TODO: don't sync while sync is in progress
+def sync_command(source: str, destination: str, is_source_s3: bool, delete: bool):
+    """Performs a sync operation between a local folder and an S3 bucket.
+
+    Args:
+        source (str): Source path.
+        destination (str): Destination path.
+        is_source_s3 (bool): Flag to indicate if the source is an S3 bucket.
+        delete (bool): Flag to indicate if the sync should delete files.
+    """
+    global aws_profile
     try:
-        command = f"aws s3 sync {source} {destination}"
+        if is_source_s3:
+            logging.info("Syncing from S3 to local")
+            command = f"aws s3 sync s3://{source} {destination} --profile {aws_profile}"
+        else:
+            logging.info("Syncing from local to S3")
+            command = f"aws s3 sync {source} s3://{destination} --profile {aws_profile}"
 
         if delete:
             command += " --delete"
+
         stdout, stderr = run_command(command)
 
         # Log output from aws cli
@@ -36,4 +96,41 @@ def sync_files(source: str, destination: str, delete: bool = True):
                 logging.error(line.strip())
 
     except Exception as e:
-        logging.error(f"Error syncing from {source} to {destination}: {e}")
+        logging.error(f"Sync error: {e}")
+
+
+def initial_sync(
+    local_folder: str, s3_bucket: str, delete: bool, initial_sync_source: str = "auto"
+):
+    """Performs the initial sync between a local folder and an S3 bucket based on the specified direction.
+
+    Args:
+        local_folder (str): Path to the local folder.
+        s3_bucket (str): Name of the S3 bucket.
+        delete (bool): Flag to indicate if the sync should delete files.
+        initial_sync_source (str): Direction for initial sync ('auto', 'local', or 's3').
+    """
+    if initial_sync_source == "auto":
+        last_modified_local = get_last_modified_local(local_folder)
+        last_modified_s3 = get_last_modified_s3(s3_bucket)
+
+        if last_modified_local > last_modified_s3:
+            sync_command(local_folder, s3_bucket, False, delete)
+        elif last_modified_s3 > last_modified_local:
+            sync_command(s3_bucket, local_folder, True, delete)
+
+    elif initial_sync_source == "local":
+        sync_command(local_folder, s3_bucket, False, delete)
+
+    elif initial_sync_source == "s3":
+        sync_command(s3_bucket, local_folder, True, delete)
+
+    else:
+        raise ValueError("Invalid initial_sync_source")
+
+
+def periodic_sync(local_folder: str, s3_bucket: str, delete: bool, sync_interval: int):
+    while True:
+        sync_command(local_folder, s3_bucket, False, delete)
+        sync_command(s3_bucket, local_folder, True, delete)
+        time.sleep(sync_interval)
